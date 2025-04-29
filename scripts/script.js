6b55e234a03ff5d0162d709e43a6e84cb1439eae
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-04-29 23:53:49 +0200

    work on

--- scripts/script.js
+++ scripts/script.js
@@ -32,11 +32,17 @@ function includeHTML(callback) {
 function init() {
   includeHTML(function() {
       renderInfoTxt();
+      renderConsultation();
       renderPriceList();
       renderMe();
   });
 }
 
+  function renderConsultation() {
+    let contentRef = document.getElementsByTagName('consul');
+    contentRef.innerHTML = "";
+    contentRef.innerHTML += getConsulTemplate();
+  }
 
   function renderInfoTxt() {
     let contentRef = document.getElementById('infoText');
@@ -68,90 +74,3 @@ function init() {
     contentRef.innerHTML = "";
     contentRef.innerHTML += aboutMeTemplate();
   }
-
-  function getInfoTemplate() {
-
-    return `
-        <div class="infoContent">
-            <h2>Meine Leisungen für ihre "energetischen" Fragen und Wünsche.</h2>
-            <ul>
-                <li>Verbrauchs und Bedarfsausweise</li>
-                <li>Energetische Gebäudebeurteilung mit unverbindlichen Sanierungstipps</li>
-                <li>Rechnerische energetische Gebäudebeurteilung mit Verlustanalyse</li>
-                <li>Individueller Sanierungsfahrplan (ISFP)</li>
-                <li>Heizlastberechnung</li>
-                <li>energetische Baubegleitung</li>
-                <li>Vorträge zum Thema Energieberatung inkl. Ihrer Fragen</li>
-            </ul>
-
-            <p class="infoPara">Ihr Thema war nicht dabei? Sie haben noch Fragen?</p>
-            <a class="infoLink" href="#" src="">Dann kontacktieren Sie uns</a>
-        </div>
-    `;
-  }
-
-  function getPriceTemplate(pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, pos10) {
-
-    return `
-      <div class="priceContent">
-        <h2>Weil mir Transperenz und Verbindlichkeit wichtig ist.</h2>
-        <div class="priceList">
-          <div class="priceBox">
-            <h3>Verbrauchsausweise</h3>
-            <p class="pricePara">Grundpreis ${pos1}</p>
-            <p class="pricePara">je m<sup>2</sup> ${pos2}</p>
-          </div>
-          <div class="priceBox">
-            <h3>Bedarfsausweis</h3>
-            <p class="pricePara">mit aktuellen Gebäudedaten ${pos3}</p>
-            <p class="pricePara">mit Datenaufnehme ${pos4}</p>
-          </div>
-          <div class="priceBox">
-            <h3>Unverbindliche energetische Beurteiling</h3>
-            <p>vor Ort < 1h ${pos5}</p>
-          </div>
-          <div class="priceBox">
-            <h3>Energetische Gebäudeberechnung mit Protokolierung</h3>
-            <p>Mit unverbindlichen Sanierungsempfehlungen ${pos6}</p>
-          </div>
-          <div class="priceBox">
-            <h3>Individueller Sanierungsfahrplan(ISFP)</h3>
-            <p> <3 Wocheneinheiten ${pos7}</p>
-            <p> <6 Wocheneinheiten ${pos8}</p>
-            <p>je weiter Wocheneinheit +${pos9}</p>
-          </div>
-          <div class="priceBox">
-            <h3>Einzelmaßnahmen für Förderungen ${pos10}</h3>
-          </div>
-          <div class="priceFooter">
-            <a class="priceLink">weitere Tätigkeiten auf Anfrage</a>
-            <i>Preise können sich je nach zeitlichem oder erhötem Aufwand ändern.</i>
-          </div>
-        </div>
-      </div>
-    `;
-  }
-
-function aboutMeTemplate() {
-
-  return`
-    <div class="meTxt">
-    <h3>Titel für diesen Absatz</h3>
-      <p class="para1">
-        Ich bin seit 2014 als Schornsteinfeger tätig und hab im Jahr 2023 erfolgreich meine Meisterprüfung abgeschlossen. 
-      </p>
-      <p class="para2">
-        Mein Interesse an den Themen Sanierung, Energiewende und Nachhaltigkeit veranlasste mich, wärend meiner Meisterausbildung 
-        auch die Qualifikation als Gebäudeenergieberater zu erwerben. Ein Jahr später entschied ich mich mein eigenes Unternehmen zu gründen, 
-        das seit dem 15. Juli 2024 für meine Kunden geöffnet ist.
-      </p>
-      <p>
-        Für Mitte 2025 plane ich ebenfalls einen eigenen Bezirk asl Schornsteinfegermeister zu Übernehmen.
-      </p>
-      <p>
-        Mein privates Glück habe ich als Ehemann und Vater zweier wundervollen Töchter gefunden. Als Familie verbringen wir viel Zeit in unserem 
-        Kleingarten um unsere Energie wieder aufzutanken. Zum Ausgleich mache ich Musik und fahre gerne mal Motorrad.
-      </p>
-    </div>
-  `;
-}
---
+++ scripts/templates.js
@@ -0,0 +1,111 @@
+function getInfoTemplate() {
+
+    return `
+        <div class="infoContent">
+            <h2>Meine Leisungen für ihre "energetischen" Fragen und Wünsche.</h2>
+            <ul>
+                <li>Verbrauchs und Bedarfsausweise</li>
+                <li>Energetische Gebäudebeurteilung mit unverbindlichen Sanierungstipps</li>
+                <li>Rechnerische energetische Gebäudebeurteilung mit Verlustanalyse</li>
+                <li>Individueller Sanierungsfahrplan (ISFP)</li>
+                <li>Heizlastberechnung</li>
+                <li>energetische Baubegleitung</li>
+                <li>Vorträge zum Thema Energieberatung inkl. Ihrer Fragen</li>
+            </ul>
+
+            <p class="infoPara">Ihr Thema war nicht dabei? Sie haben noch Fragen?</p>
+            <a class="infoLink" href="#" src="">Dann kontacktieren Sie uns</a>
+        </div>
+    `;
+  }
+
+  function getPriceTemplate(pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9, pos10) {
+
+    return `
+      <div class="priceContent">
+        <h2>Weil mir Transperenz und Verbindlichkeit wichtig ist.</h2>
+        <div class="priceList">
+          <div class="priceBox">
+            <h3>Verbrauchsausweise</h3>
+            <p class="pricePara">Grundpreis ${pos1}</p>
+            <p class="pricePara">je m<sup>2</sup> ${pos2}</p>
+          </div>
+          <div class="priceBox">
+            <h3>Bedarfsausweis</h3>
+            <p class="pricePara">mit aktuellen Gebäudedaten ${pos3}</p>
+            <p class="pricePara">mit Datenaufnehme ${pos4}</p>
+          </div>
+          <div class="priceBox">
+            <h3>Unverbindliche energetische Beurteiling</h3>
+            <p>vor Ort < 1h ${pos5}</p>
+          </div>
+          <div class="priceBox">
+            <h3>Energetische Gebäudeberechnung mit Protokolierung</h3>
+            <p>Mit unverbindlichen Sanierungsempfehlungen ${pos6}</p>
+          </div>
+          <div class="priceBox">
+            <h3>Individueller Sanierungsfahrplan(ISFP)</h3>
+            <p> <3 Wocheneinheiten ${pos7}</p>
+            <p> <6 Wocheneinheiten ${pos8}</p>
+            <p>je weiter Wocheneinheit +${pos9}</p>
+          </div>
+          <div class="priceBox">
+            <h3>Einzelmaßnahmen für Förderungen ${pos10}</h3>
+          </div>
+          <div class="priceFooter">
+            <a class="priceLink">weitere Tätigkeiten auf Anfrage</a>
+            <i>Preise können sich je nach zeitlichem oder erhötem Aufwand ändern.</i>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+function aboutMeTemplate() {
+
+  return`
+    <div class="meTxt">
+    <h3>Titel für diesen Absatz</h3>
+      <p class="para1">
+        Ich bin seit 2014 als Schornsteinfeger tätig und hab im Jahr 2023 erfolgreich meine Meisterprüfung abgeschlossen. 
+      </p>
+      <p class="para2">
+        Mein Interesse an den Themen Sanierung, Energiewende und Nachhaltigkeit veranlasste mich, wärend meiner Meisterausbildung 
+        auch die Qualifikation als Gebäudeenergieberater zu erwerben. Ein Jahr später entschied ich mich mein eigenes Unternehmen zu gründen, 
+        das seit dem 15. Juli 2024 für meine Kunden geöffnet ist.
+      </p>
+      <p>
+        Für Mitte 2025 plane ich ebenfalls einen eigenen Bezirk asl Schornsteinfegermeister zu Übernehmen.
+      </p>
+      <p>
+        Mein privates Glück habe ich als Ehemann und Vater zweier wundervollen Töchter gefunden. Als Familie verbringen wir viel Zeit in unserem 
+        Kleingarten um unsere Energie wieder aufzutanken. Zum Ausgleich mache ich Musik und fahre gerne mal Motorrad.
+      </p>
+    </div>
+  `;
+}
+
+function getConsulTemplate() {
+
+  return`
+    <div class="consulContent">
+      <h2>Energieberatng</h2>
+      <h3>Was macht ein Gebäudeenergieberater eigentlich?</h3>
+      <p class="consulPara">
+        In der Heutigen Zeit ist es eine Herausforderung Thema Haussanierung den Überblick zu erhalten.
+      </p>
+      <p>
+        Förderung, Gesetze und Standarts unterliegen ständigen Veränderungen. Zudem kursieren Fehlinformation 
+        über Anlagetechnick und Sanierung, was es schwierig macht die Warheit zu erkennen
+      </p>
+      <p>
+        Um in diesem Chaos klarheit zu schaffen und ihnen als Kunden fachgerecht Informationen zubieten, 
+        stehe ich ihnen als Energieberater zur Verfügung.
+      </p>
+      <p>
+        Darüber hinaus haben Sie die Möglichkeit durch eine Zusammenarbeit mit mir, als gelisteten Energie-Effizienz_Experten, 
+        Ihre Sanierungsmaßnahmen staatlch förderfähig zu machen und so Geld zu sparen.
+      </p>
+    </div>
+  `;
+}
